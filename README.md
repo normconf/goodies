98d54ef945c86d4439af84f2397fe646fced446c
bjl <blabaschin@MacBook-Pro.local>
2022-10-10 17:03:26 -0700

    get normconf api up there

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ Using latent-space embeddings we synthesize every arxiv deep learning finding in
 | normconf | api.normconf.com/normconf | ASCII Goodness | 
 | random_goodies | api.normconf.com/random_goodies | Random Goodness | 
 | allthegoodies | api.normconf.com/allthegoodies | Too Much Goodness | 
+
+
+Ideas
+
+1 a randomized response of our curated list of blog posts,
+2 same as 1 but shitposts,
+3 pizza-delimited values and maybe a link to that twitter conversation about delimiters
--- app/normconf.py
+++ app/normconf.py
@@ -1,11 +1,12 @@
+from io import BytesIO
+from os import listdir
+from pathlib import Path
+from random import choice
+from zipfile import ZIP_DEFLATED, ZipFile
+
 import structlog
 from fastapi import APIRouter
 from fastapi.responses import FileResponse, StreamingResponse
-from pathlib import Path
-from os import listdir
-from random import choice
-from io import BytesIO
-from zipfile import ZipFile, ZIP_DEFLATED
 
 log = structlog.get_logger()
 
@@ -13,7 +14,7 @@ normie_router = APIRouter(prefix="/normconf", responses={404: {"description": "A
 
 goodies_path = 'app/goodies/'
 surprises = [goodies_path + file for file in listdir(Path('app/goodies/'))]
-
+vicki = [url for url in open('app/goodies/queen.txt').read().splitlines()]
 
 @normie_router.get('/normconf')
 def get_normconf():
@@ -27,7 +28,6 @@ def random_goodies():
     """Return random goodie file from goodies directory
     """
     goodie = choice(surprises)
-    print(goodie)
 
     if goodie.endswith('.png'):
         return FileResponse(goodie, media_type='image/png', filename=goodie[len(goodies_path):])
@@ -50,3 +50,11 @@ def get_goodies():
                              media_type="application/x-zip-compressed", 
                              headers = { "Content-Disposition": f"attachment; filename=allthegoodies.zip"}
                             )
+
+@normie_router.get('/queen_vicki')
+def queen():
+    """Return link from Vicki's site
+    """
+    site = choice(vicki)
+
+    return site
